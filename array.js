0032c37899fedef56d303a2bb4eb0785595b4fab
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-01-28 18:19:05 +0600

    variable using array

---
+++ array.js
@@ -0,0 +1,11 @@
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 442;
+// const num4 = 442;
+
+// const numbers = 122215464878777;
+// (), {}, []
+const numbers = [12, 22, 54, 65];
+const friends = ['Abul', 'Kabul', 'Babul', 'Bulbul'];
+const passed = [true, false, true, false];
+const mixedArray = [12, 'Alahbad', true, 45.566];
